52c89eedcae05be4e940a563e7e4188d434bde19
Yao0454 <yaoyifeng20070524@outlook.com>
2025-12-29 17:45:22 +0800

    Update

--- Python Experiment/Experiment 8/Test 1.py
+++ Python Experiment/Experiment 8/Test 1.py
@@ -23,7 +23,7 @@ class Student:
         
     # 实例方法： 返回所有科目成绩
     def get_all_scores(self) -> str:
-        result = f"===={self.name}的所有科目成绩===="
+        result = f"----{self.name}的所有科目成绩----\n"
         for subject, score in self.scores.items():
             result += f"{subject}: {score}\n"
         return result
@@ -44,10 +44,8 @@ class StudentManager:
     def find_student(self, student_id: int):
         for student in self.students:
             if student.student_id == student_id:
-                return student
-            else:
-                return student.empty_ob()
-        return None
+                return student.name
+        return Student.empty_ob()
         
     # 实例方法： 班级所有学生的平均分
     def get_class_average(self) -> float:
@@ -57,7 +55,39 @@ class StudentManager:
         return sum(avg_scores) / len(avg_scores)
     
     # 实例方法： 所有学生信息及成绩
-    def sholl_all_students(self)  -> None:
-        
+    def show_all_students(self) -> None:
+        for student in self.students:
+            print(student.get_info())
+            print(student.get_all_scores())
+            
+def main() -> None:
+    student1 = Student("姚奕枫", 202521095054, 18, {})
+    student2 = Student("舒天宇", 202521095025, 19, {})
+    student3 = Student("张晋铭", 202521095060, 19, {})
     
     
+    student1.add_score("语文", 74.0)
+    student1.add_score("数学", 121.0)
+    student1.add_score("英语", 134.0)
+    student2.add_score("语文", 112.0)
+    student2.add_score("数学", 108.0)
+    student2.add_score("英语", 131.0)
+    student3.add_score("语文", 121.0)
+    student3.add_score("数学", 109.0)
+    student3.add_score("英语", 128.0)
+    
+    sm = StudentManager()
+    
+    sm.add_student(student1)
+    sm.add_student(student2)
+    sm.add_student(student3)
+    
+    id1: int = 202521095025
+    print(f"学号为{id1}的学生为{sm.find_student(id1)}")
+    
+    print(f"班级平均分为{sm.get_class_average():.2f}")
+    
+    sm.show_all_students()
+    
+if __name__ == "__main__":
+    main()
